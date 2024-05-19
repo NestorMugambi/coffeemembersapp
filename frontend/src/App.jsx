d773f2ff20caebbc72daf2ad1c052ba03d44c05f
NestorMugambi <arielnestor14@gmail.com>
2024-05-20 00:07:27 +0300

    added div height

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -71,7 +71,7 @@ export default function App() {
           </div>
          
         </div>  
-        <div className="max-w-4xl max-h-fit">
+        <div className="max-w-4xl max-h-72">
           <h2 className="text-3xl font-bold tracking-tight text-gray-900 sm:text-4xl">Meet our Team</h2>
           <p className="mt-6 text-lg leading-8 text-gray-600">
           We are a dynamic group of individuals who are passionate about what we do and dedicated to delivering the best results for our clients
